60e79c53435c33ff694b54c519f4eeb12d816bb5
LottaMe <56435437+LottaMe@users.noreply.github.com>
2021-11-15 17:43:33 +0100

    disable register

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
-    path('auth/register/', include('rest_auth.registration.urls')),
+    #path('auth/register/', include('rest_auth.registration.urls')),
     path('auth/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
